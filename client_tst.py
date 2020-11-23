e96322bf06d4b1f69059d0463180f3f4da75db25
宇野拓磨 <>
2020-11-23 12:14:02 +0900

    マージして実機テストできるようにした

--- client_tst.py
+++ client_tst.py
@@ -1,11 +1,6 @@
 import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # サーバを指定
-<<<<<<< HEAD
-    #
     s.connect(('127.0.0.1', 50007))
-=======
-    s.connect(('127.0.0.1', 8080))
->>>>>>> main
     # サーバにメッセージを送る
     s.sendall(b'1')
