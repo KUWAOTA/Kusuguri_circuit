9853fe3d4f119f94b9d802ceafd84a219d73d21f
宇野拓磨 <>
2020-11-23 11:41:10 +0900

    サーバー送れなくなった

--- client_tst.py
+++ client_tst.py
@@ -1,6 +1,6 @@
 import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # サーバを指定
-    s.connect(('127.0.0.1', 12345))
+    s.connect(('127.0.0.1', 8080))
     # サーバにメッセージを送る
     s.sendall(b'1')
